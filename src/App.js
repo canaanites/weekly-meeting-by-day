dc14f45324bfe786c7eb7d5e77d044707e3b0dba
Moe Kanan <mk.93@live.com>
2020-09-05 18:57:07 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 function App() {
+  const dayOptions = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]; 
+  const hourOptions = Array.from(Array(12), (_, i) => `${i + 1}:00`)
+  const minuteOptions = Array.from(Array(59), (_, i) => `00:${i + 1}`)
+
+
+  const [time, setTime] = useState();
+
+  const _onSelect = (date, key) => {
+    return setTime({[key]: date.value});
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Dropdown
+          options={dayOptions}
+          onChange={_onSelect}
+          value={dayOptions[0]}
+          placeholder="Select Day"
+        />
+        <Dropdown
+          options={hourOptions}
+          onChange={_onSelect}
+          value={"1:00"}
+          placeholder="Select Hour"
+        />
+        <Dropdown
+          options={minuteOptions}
+          onChange={_onSelect}
+          value={"00:00"}
+          placeholder="Select Minute"
+        />
       </header>
     </div>
   );
